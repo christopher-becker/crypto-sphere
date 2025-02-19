b6bce12103b7e9edaafebbdc528e77ef23727935
Christopher Becker <thechristopherbecker@gmail.com>
2025-02-19 12:47:58 +0200

    [master] added a width to crypto sphere logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="flex gap-4 sm:gap-8 items-center max-w-3xl m-auto">
         <nav className="flex gap-4 flex-1">
           <Link to={"/"} className="flex gap-3 items-center">
-            <img className="h-8" src="/logo.png" alt="Crypto Sphere Logo" />
+            <img className="h-8 w-8" src="/logo.png" alt="Crypto Sphere Logo" />
             <strong className="hidden sm:flex">Crypto-Sphere</strong>
           </Link>
         </nav>
