af544a66959254c603dfa7ddccdbc809c7476377
Christopher Becker <thechristopherbecker@gmail.com>
2025-02-28 09:58:04 +0200

    [master] removed interval fetches to make way for websocket integration

--- src/core/context/coinGecko.context.tsx
+++ src/core/context/coinGecko.context.tsx
@@ -4,10 +4,9 @@ import {
   useEffect,
   useState,
   useMemo,
-  useRef,
 } from "react";
 import { getCoinsList } from "../../api/coinGeckoAPI";
-import { CoinDirection, CoinType } from "../types/coinGecko.type";
+import { CoinType } from "../types/coinGecko.type";
 
 interface CoinGeckoContextType {
   list: CoinType[];
@@ -22,48 +21,19 @@ export const CoinGeckoContext = createContext<CoinGeckoContextType>({
 export const CoinGeckoProvider = ({ children }: PropsWithChildren<unknown>) => {
   const [list, setList] = useState<CoinType[]>([]);
   const [isLoadingList, setIsLoadingList] = useState<boolean>(true);
-  const listRef = useRef<CoinType[]>([]);
 
   useEffect(() => {
     const fetchCoinList = async () => {
       try {
         const data: CoinType[] = await getCoinsList();
-        if (data) {
-          const newData: CoinType[] = [];
-          data.forEach((newCoin) => {
-            const currentCoin = listRef.current?.find(
-              (coin) => coin.id == newCoin.id
-            );
-            if (!currentCoin) {
-              newData.push({ ...newCoin, direction: CoinDirection.NONE });
-              return;
-            }
-            if (newCoin.current_price > currentCoin.current_price) {
-              newData.push({ ...newCoin, direction: CoinDirection.UP });
-            }
-            if (newCoin.current_price < currentCoin.current_price) {
-              newData.push({ ...newCoin, direction: CoinDirection.DOWN });
-            }
-            if (newCoin.current_price == currentCoin.current_price) {
-              newData.push({ ...newCoin, direction: CoinDirection.NONE });
-            }
-          });
-          setList(() => newData);
-          listRef.current = newData;
-        }
+        setList(() => data);
       } catch (error) {
         console.error("Failed to fetch coin list:", error);
       }
       setIsLoadingList(false);
     };
 
-    const interval = setInterval(() => {
-      fetchCoinList();
-    }, 61000);
-
     fetchCoinList();
-
-    return () => clearInterval(interval);
   }, []);
 
   const contextValue = useMemo(
